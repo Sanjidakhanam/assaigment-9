e41c29b4f040f59a264349ee10f2ab5a8ac83e4a
unknown <you@example.com>
2024-10-15 14:56:06 +0600

    first commit

--- length.js
+++ length.js
@@ -1,2 +1,9 @@
 const numbers = [2,5,7,11,14,12,16,19];
 console.log(numbers.length);
+
+
+
+const team = ["tanni","mim","sanjida","jinia",]
+//const x= team,includes("mim")
+// console.log(x)
+console.log(Array.isArray(team));
